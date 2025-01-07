d6d02f0971678feb3ec4b829a69614de2d132b5a
levitacija <dudukin.matvey@mail.ru>
2025-01-07 14:44:55 +0300

    0.6 refactoring 2

--- main.go
+++ main.go
@@ -7,18 +7,6 @@ import (
 	"strings"
 )
 
-func main() {
-	http.HandleFunc("/user-agent", userAgentHandler)
-	http.HandleFunc("/echo/", echoHandler)
-	http.HandleFunc("/", rootHandler)
-
-	fmt.Println("start with port 6969")
-	if err := http.ListenAndServe(":6969", nil); err != nil {
-		fmt.Println("Error starting server: ", err.Error())
-		os.Exit(1)
-	}
-}
-
 func echoHandler(w http.ResponseWriter, r *http.Request) {
 	message := strings.TrimPrefix(r.URL.Path, "/echo/")
 	if message == "" {
@@ -49,4 +37,35 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	w.WriteHeader(http.StatusOK)
+}
+
+func returnFileHandler(w http.ResponseWriter, r *http.Request) {
+	filePath := "tmp/" + strings.TrimPrefix(r.URL.Path, "/file/")
+	fileInfo, err := os.Stat(filePath)
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
+	content, err := os.ReadFile(filePath)
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
+	w.Header().Set("Content-Type", "application/octet-stream")
+	w.Header().Set("Content-Length", fmt.Sprintf("%d", fileInfo.Size()))
+	w.WriteHeader(http.StatusOK)
+	w.Write(content)
+}
+
+func main() {
+	http.HandleFunc("/user-agent", userAgentHandler)
+	http.HandleFunc("/echo/", echoHandler)
+	http.HandleFunc("/", rootHandler)
+	http.HandleFunc("/file/", returnFileHandler)
+
+	fmt.Println("start with port 6969")
+	if err := http.ListenAndServe(":6969", nil); err != nil {
+		fmt.Println("Error starting server: ", err.Error())
+		os.Exit(1)
+	}
 }
